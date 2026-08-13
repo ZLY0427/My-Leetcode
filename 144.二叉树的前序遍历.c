4ce6fce89e4f9c0ba22c8f612c4248e59e5ba2ba
ZLY0427 <3954416531@qq.com>
2026-08-13 16:28:12 +0800

    leetcode144(Stack).

---
+++ 144.二叉树的前序遍历.c
@@ -0,0 +1,123 @@
+/*
+ * @lc app=leetcode.cn id=144 lang=c
+ *
+ * [144] 二叉树的前序遍历
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+#include <stdlib.h>
+
+#ifdef RUN_LOCATED_VSCODE
+struct TreeNode {
+    int val;
+    struct TreeNode *left;
+    struct TreeNode *right;
+};
+#endif
+
+struct StackNode {
+    struct TreeNode* node;
+    struct StackNode* next;
+};
+
+struct Stack {
+    struct StackNode* top;
+    int count;
+};
+
+struct Stack* create_Stack();
+void push_Stack(struct Stack* stack, struct TreeNode* node);
+struct TreeNode* pop_Stack(struct Stack* stack);
+void delete_Stack(struct Stack* stack);
+
+int* preorderTraversal(struct TreeNode* root, int* returnSize)
+{
+    *returnSize = 0;
+    if (!root) return NULL;
+
+    int* buffer = (int*)malloc(sizeof(int) * 100);
+    if (!buffer) return NULL;
+
+    struct Stack* stack = create_Stack();
+    if (!stack) return NULL;
+
+    push_Stack(stack, root);
+    while(stack -> count > 0)
+    {
+        struct TreeNode* cur = pop_Stack(stack);
+        buffer[(*returnSize)++] = cur -> val;
+        
+        if(cur -> right != NULL) push_Stack(stack, cur -> right);
+        if(cur -> left != NULL) push_Stack(stack, cur -> left);
+    }
+
+    delete_Stack(stack);
+
+    int* result = (int*)realloc(buffer, sizeof(int) * (*returnSize));
+    
+    return result;
+}
+
+struct Stack* create_Stack()
+{
+    struct Stack* stack = (struct Stack*)malloc(sizeof(struct Stack));
+    if (!stack) return NULL;
+
+    stack -> top = NULL;
+    stack -> count = 0;
+
+    return stack;
+}
+
+void push_Stack(struct Stack* stack, struct TreeNode* node)
+{
+    if (!stack) return ;
+    struct StackNode* stack_node = (struct StackNode*)malloc(sizeof(struct StackNode));
+    if (!stack_node) return ;
+
+    stack_node -> node = node;
+    stack_node -> next = stack -> top;
+    
+    stack -> top = stack_node;
+    ++(stack -> count);
+}
+
+struct TreeNode* pop_Stack(struct Stack* stack)
+{
+    if (!stack) return NULL;
+    if (stack -> count <= 0) return NULL;
+
+    struct StackNode* stack_node = stack -> top;
+    struct TreeNode* node = stack_node -> node;
+
+    stack -> top = stack_node -> next;
+    --(stack -> count);
+
+    free(stack_node);
+
+    return node;
+}
+
+void delete_Stack(struct Stack* stack)
+{
+    if (!stack) return ;
+
+    while (stack -> count > 0)
+        pop_Stack(stack);
+
+    free(stack);
+}
+
+// @lc code=end
+
